a95c85dac989215fd64eb1b5fa3dc23fc1bf4fda
Enrique Matta-Rodriguez <enrique.j.matta@gmail.com>
2025-04-04 12:32:22 -0500

    checkpoint

--- app/src/main/java/edu/utap/AuthenticatedRoutes.kt
+++ app/src/main/java/edu/utap/AuthenticatedRoutes.kt
@@ -3,7 +3,7 @@ package edu.utap
 object AuthenticatedRoutes {
     const val DASHBOARD = "dashboard"
     const val DISCUSSIONS = "discussions"
-    const val DISCUSSIONS_THREAD = "discussions_thread"
+    const val DISCUSSIONS_THREAD = "discussions/{threadId}"
     const val PROFILE = "profile"
     const val FLOOD_REPORT = "flood_report"
 }
