56c0c29fa155899e34d0b4527023bf3e145c4cbd
Enrique Matta-Rodriguez <enrique.j.matta@gmail.com>
2025-04-08 08:25:09 -0500

    checkpoint

--- app/src/main/java/edu/utap/flood/di/DiscussionViewModelFactory.kt
+++ app/src/main/java/edu/utap/flood/di/DiscussionViewModelFactory.kt
@@ -1,5 +1,6 @@
 package edu.utap.flood.di
 
+import android.util.Log
 import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
@@ -19,13 +20,37 @@ class DiscussionViewModelFactory private constructor() : ViewModelProvider.Facto
             return object : ViewModelProvider.Factory {
                 @Suppress("UNCHECKED_CAST")
                 override fun <T : ViewModel> create(modelClass: Class<T>): T {
-                    val discussionRepository = DiscussionModule.provideDiscussionRepository()
-                    val authViewModel = edu.utap.auth.di.ViewModelFactory(context)
-                        .create(AuthViewModel::class.java) as AuthViewModelInterface
-                    return DiscussionViewModel(
-                        discussionRepository = discussionRepository,
-                        authViewModel = authViewModel
-                    ) as T
+                    val discussionRepository: DiscussionRepositoryInterface = try {
+                        DiscussionModule.provideDiscussionRepository()
+                    } catch (e: Exception) {
+                        Log.e("DiscussionVMFactory", "Failed to provide DiscussionRepository", e)
+                        throw e
+                    }
+
+                    val authViewModelAny: Any = try {
+                        edu.utap.auth.di.ViewModelFactory(context)
+                            .create(AuthViewModel::class.java)
+                    } catch (e: Exception) {
+                        Log.e("DiscussionVMFactory", "Failed to create AuthViewModel", e)
+                        throw e
+                    }
+
+                    val authViewModel: AuthViewModelInterface = try {
+                        authViewModelAny as AuthViewModelInterface
+                    } catch (e: Exception) {
+                        Log.e("DiscussionVMFactory", "Failed to cast AuthViewModel to AuthViewModelInterface", e)
+                        throw e
+                    }
+
+                    return try {
+                        DiscussionViewModel(
+                            discussionRepository = discussionRepository,
+                            authViewModel = authViewModel
+                        ) as T
+                    } catch (e: Exception) {
+                        Log.e("DiscussionVMFactory", "Failed to instantiate DiscussionViewModel", e)
+                        throw e
+                    }
                 }
             }
         }
